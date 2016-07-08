349861fc75fd1ad35a084702b8dd6a9b906a68fb
carsonlevine <carsonglevine@me.com>
2016-07-08 15:58:10 -0400

    first commit

---
+++ ObjectOrientedLabels/CustomLabel.swift
@@ -0,0 +1,16 @@
+//
+//  CustomButton.swift
+//  ObjectOrientedLabels
+//
+//  Created by flatironstudent on 3/5/16.
+//  Copyright © 2016 flatironstudent. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class CustomLabel {
+    
+
+}
---
+++ ObjectOrientedLabels/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  ObjectOrientedLabels
+//
+//  Created by flatironstudent on 3/5/16.
+//  Copyright © 2016 flatironstudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+ 
+}
+
